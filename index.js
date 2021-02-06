a2cce630d0e98322497bb14ae3635ff9cdd2eaf7
Daniel Subiabre <subiabrewd@gmail.com>
2021-02-06 01:04:53 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const bot = require('./src/bot')
+
+// Login
+client.on('ready', () => {
+    bot.setClient(client)
+    bot.log.info(`Bot connected to ${client.user.tag}`)
+})
+
+// Bot actions
+client.on('message', async (msg) => {
+    await bot.ping(msg)
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
